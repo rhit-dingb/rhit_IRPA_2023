6fc6cf1023d0e91448e04cbbcbc211da62e44416
rhit-dingb <dingb@rose-hulman.edu>
2022-10-12 11:23:38 -0400

    found something to use at this moment

---
+++ chatbot_app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Basic from "./component/Basic";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>IRPA ChatBot</h1>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
--- chatbot_app/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ReactDOM } from "react";
-import App from './App';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
