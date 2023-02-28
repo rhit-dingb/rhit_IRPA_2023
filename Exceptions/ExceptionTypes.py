7a389079c61f748f6374c0757fdc491eedcb9a80
Travis544 <zhengy6@rose-hulman.edu>
2023-02-27 22:07:41 -0500

    updated cohort training data

--- Exceptions/ExceptionTypes.py
+++ Exceptions/ExceptionTypes.py
@@ -3,7 +3,6 @@ from enum import Enum
 from Exceptions.ExceptionMessages import COHORT_YEAR_VALUE_ERROR_MESSAGE_FORMAT
 from enum import Enum, auto
 class ExceptionTypes(Enum):
-    CohortByYearEntityValueException = auto()
     NoDataFoundForAcademicYearException = auto()
     NoDataFoundAtAll = auto()
     NoSparseMatrixDataAvailableForGivenIntent = auto()
